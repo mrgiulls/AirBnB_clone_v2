01735f810bf08d44240aa70f205fa8f616cf0407
Giuliano Flores Mesías <72042622+mrgiulls@users.noreply.github.com>
2022-05-02 17:42:19 -0500

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -20,3 +20,4 @@ def do_clean(number=0):
     local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
     path = '/data/web_static/releases'
     run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+    
